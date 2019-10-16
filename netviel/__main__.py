03d70845432e1aa48e7410db80da54bd739c9725
David Straub <david.straub@tum.de>
2019-10-16 14:20:46 +0200

    Allow changing port in main script

--- netviel/__main__.py
+++ netviel/__main__.py
@@ -2,5 +2,13 @@ from .api import create_app
 
 
 if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser(description="Run the netviel app locally")
+    parser.add_argument(
+        "--port", type=int, help="Port to use (defaults to 5000)", default=5000
+    )
+    args = parser.parse_args()
+
     app = create_app()
-    app.run()
+    app.run(port=args.port)
